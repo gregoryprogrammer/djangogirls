34a0b2cd6e154f2fab5fb606a8f72aec1abd2529
Grzegorz Gwozdz <gregoryprogrammer@gmail.com>
2017-09-16 17:41:06 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+def moja_funkcja(number, second):
+	print(4)
+	print('siedem')
+	print(number * 4)
+
+def dodaj(a, b):
+	s = a + b
+	return s
+
+result = moja_funkcja(7, 'ala')
+print(result)
+
+suma = dodaj(4, 8)
+print(suma)
